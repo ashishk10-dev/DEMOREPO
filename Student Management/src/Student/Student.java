d8ee04238a4175f9e47ed483a3f87730db08005f
DELL <DELL@DESKTOP-PP11UR7.mshome.net>
2024-12-27 16:32:19 +0530

    added show method

--- Student Management/src/Student/Student.java
+++ Student Management/src/Student/Student.java
@@ -3,4 +3,8 @@ package Student;
 public class Student {
 int rollno=101;
 String name="BHVGVGVGV";
+
+public void show(){
+	System.out.println("GOOD MORNING...!");
+}
 }
--- Student Management/src/Student/Test.java
+++ Student Management/src/Student/Test.java
@@ -5,5 +5,7 @@ public static void main(String[] args) {
 	Student s=new Student();
 	System.out.println(s.rollno);
 	System.out.println(s.name);
+	
+	s.show();
 }
 }
