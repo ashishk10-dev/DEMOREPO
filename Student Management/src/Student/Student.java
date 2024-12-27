5a6d235070783233261fa1775504a5a38b37c397
DELL <DELL@DESKTOP-PP11UR7.mshome.net>
2024-12-27 16:29:31 +0530

    First Commit

---
+++ Student Management/src/Student/Student.java
@@ -0,0 +1,6 @@
+package Student;
+
+public class Student {
+int rollno=101;
+String name="BHVGVGVGV";
+}
---
+++ Student Management/src/Student/Test.java
@@ -0,0 +1,9 @@
+package Student;
+
+public class Test {
+public static void main(String[] args) {
+	Student s=new Student();
+	System.out.println(s.rollno);
+	System.out.println(s.name);
+}
+}
